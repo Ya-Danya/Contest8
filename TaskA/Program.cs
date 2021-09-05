b49b744928460c59197bb86992e603095d2d7500
Dmitry Samsonenko <samd@nsi-telecom.ru>
2021-09-05 17:50:26 +0300

    Добавьте файлы проекта.

---
+++ TaskA/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+delegate double Calculate(int n);
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int x = int.Parse(ReadLine());
+
+        Calculate calculate = x =>
+        {
+            double a = 0;
+            double b = 0;
+            for (double i = 1.0; i <= 5.0; i++)
+            {
+                b = 1;
+                for (double j = 1.0; j <= 5.0; j++)
+                {
+                    b *= ((i + 42) * x) / (j * j);
+                }
+                a += b;
+            }
+            return Math.Round(a, 3);
+        };
+
+        Write(calculate(x));
+    }
+}
---
+++ TaskC/IntWrapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class IntWrapper
+{
+    private int number;
+
+    public int Number
+    {
+        get { return number; }
+    }
+
+    public IntWrapper(int number)
+    {
+        this.number = number;
+    }
+
+    public uint FindNumberLength()
+    {
+        int n = number;
+        uint count = 0;
+        do
+        {
+            n = n % 10;
+            count++;
+        } while (n>=0);
+        return (count);
+    }
+}
